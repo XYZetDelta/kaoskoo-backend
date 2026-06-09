652a9f156c86a2d1ec57c3226e1e19caf465889b
XYZetDelta <akukamael934@gmail.com>
2026-06-09 09:37:10 +0700

    update db

--- db.js
+++ db.js
@@ -1,18 +1,15 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2")
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "", // laragon biasanya kosong
-    database: "kaoskoo_new"
-});
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL || process.env.MYSQL_URL || {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+})
 
-connection.connect((err) => {
-    if (err) {
-        console.error("Database gagal connect:", err);
-    } else {
-        console.log("MySQL Connected");
-    }
-});
+db.connect((err) => {
+  if (err) console.error("Database gagal connect:", err)
+  else console.log("Database connected!")
+})
 
-module.exports = connection;
+module.exports = db
